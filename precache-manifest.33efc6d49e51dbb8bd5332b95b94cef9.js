08ae3f396ecde1dabe68055aa101075f70070ddf
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2020-08-31 15:49:37 +0200

    Updates

---
+++ precache-manifest.33efc6d49e51dbb8bd5332b95b94cef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297bc2cc6c5ceea5bd15deb9826780e0",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "9a209b2321a32e96b9c5",
+    "url": "/Pokedex/static/css/main.bdae3c3e.chunk.css"
+  },
+  {
+    "revision": "1203cbcd5b0983cb03c7",
+    "url": "/Pokedex/static/js/2.c423f07d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.c423f07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a209b2321a32e96b9c5",
+    "url": "/Pokedex/static/js/main.a933d5ae.chunk.js"
+  },
+  {
+    "revision": "9fb1140b615de1adf3f4",
+    "url": "/Pokedex/static/js/runtime-main.e9efafc6.js"
+  }
+]);
